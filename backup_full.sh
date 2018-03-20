2cc1f975ae4b604f2f38c5d1633b9cebf94f9e83
th14golop35 <thiagoolopezz@gmail.com>
2018-03-20 17:34:13 -0300

    Add files via upload

---
+++ backup_full.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+echo "Backup full"
+
+
+echo " "
+
+dadosfull() {
+
+SRCDIR="/mnt/Dados/ARQUIVOS/" #diretórios que serão feito backup
+DSTDIR=/mnt/backup/FULL #diretório de destino do backup
+DATA=`date +%x-%k%M%S` #pega data atual
+TIME_BKCP=+30 #número de dias em que será deletado o arquivo de backup
+
+#criar o arquivo full-backup.tar no diretório de destino
+#ARQ=$DSTDIR/full-$DATA.tar.gz
+ARQ=$DSTDIR/full-$DATA.zip
+#data de inicio backup
+DATAIN=`date +%c`
+echo "Data de inicio: $DATAIN"
+
+}
+
+backupfull(){
+sync
+tar -cvf $ARQ $SRCDIR
+if [ $? -eq 0 ] ; then
+   echo "----------------------------------------"
+        echo "Backup Full concluído com Sucesso"
+   DATAFIN=`date +%c`
+   echo "Data de termino: $DATAFIN"
+   echo "Backup realizado com sucesso" >> /var/log/backup_full.log
+   echo "Criado pelo usuário: $USER" >> /var/log/backup_full.log
+   echo "INICIO: $DATAIN" >> /var/log/backup_full.log
+   echo "FIM: $DATAFIN" >> /var/log/backup_full.log
+   echo "-----------------------------------------" >> /var/log/backup_full.log
+   echo " "
+   echo "Log gerado em /var/log/backup_full.log"
+
+else
+   echo "ERRO! Backup do dia $DATAIN" >> /var/log/backup_full.log
+fi  
+}
+cp -r /var/log/backup_full.log /mnt/backup/LOG/
+procuraedestroifull(){
+
+#apagando arquivos mais antigos (a mais de 20 dias que existe)
+find $DSTDIR -name "f*" -ctime $TIME_BKCP -exec rm -f {} ";"
+   if [ $? -eq 0 ] ; then
+      echo "Arquivo de backup mais antigo eliminado com sucesso!"
+   else
+      echo "Erro durante a busca e destruição do backup antigo!"
+   fi
+}
+
+dadosfull
+backupfull
+procuraedestroifull
+
+exit 0
